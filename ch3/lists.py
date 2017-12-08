666c25fa1d8f30a1db7e4ba3f9d4cb68ac598a12
Dearle Riggs <morcanthra@gmail.com>
2017-12-08 09:58:01 -0800

    Chapter 2 complete, 3 started

---
+++ ch3/lists.py
@@ -0,0 +1,5 @@
+bicycles = ['trek', 'cannondale', 'mongoose', 'huffy']
+print(bicycles)    # Prints [trek, cannondale, mongoose, huffy]
+print(bicycles[0])  # Prints trek, the first item in the list as the the lists are zero indexed
+print(bicycles[-1]) # Prints the last item in the list, huffy
+print(bicycles[1].upper())  # Prints CANNONDALE, 2nd item in uppercase
